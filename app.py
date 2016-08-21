857fa2a31e489e9f648c07f05bed7c277d4aa372
AT <ewzarp@gmail.com>
2016-08-21 19:07:19 -0400

    first working version

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect
 
+from quandl_py_direct import get_stock_price, insert_plot_into_html
+
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,5 +14,18 @@ def main():
 def index():
   return render_template('index.html')
 
+@app.route('/plot', methods=['POST'])
+def plot():
+    stock_symbol = request.form['myvar'].upper()
+    get_stock_price(stock_symbol)
+    
+    orig_template="index.html"
+    output_html="plot.html"
+    insert_plot_into_html("templates\index.html", "templates\lines.html", "templates\plot.html") #must be called after get_stock_price()
+
+    return render_template('plot.html')
+
 if __name__ == '__main__':
+  #app.debug = True #uncomment when done debugging
+  app.config.update(TEMPLATES_AUTO_RELOAD=True)
   app.run(port=33507)
---
+++ quandl_py_direct.py
@@ -0,0 +1,76 @@
+import quandl
+import pandas as pd
+import datetime as dt
+from bokeh.plotting import figure, output_file, show, save
+
+def load_file_as_list(CURR_FILENAME):
+    """
+    returns file contents as a list where each line is a element of the list
+    """
+    inFile = open(CURR_FILENAME)
+    lines = inFile.readlines()
+    inFile.close()
+    return lines
+
+def write_string_to_file(CURR_FILENAME, string_to_write):
+    """
+    Writes a string (string_to_write) to the file SEQUENCE_FILENAME
+    """
+    inFile=open(CURR_FILENAME,'w')
+    inFile.write(string_to_write)
+    inFile.close()
+    return None
+
+def get_stock_price(stock_symbol):
+    quandl.ApiConfig.api_key = 'rxxtZcyJWMVKp4EVby_9' 
+    data = quandl.get("WIKI/" + stock_symbol)
+    # "Date" is an index (DateTime Index to be specific), not a normal column
+    # to make it a normal column, use
+    # data=data.reset_index()
+
+    #col_names=list(data.columns.values) #does not include the name of 1st "date" col
+    col_names=list(data) #shorter version of the top line (that line works just as fine, just longer)
+    #print('col names=',col_names)
+
+    date = dt.date.today()    # date.year, date.month, date.day
+    last_mo_data=data.ix[pd.datetime(date.year,date.month-1,1) : pd.datetime(date.year,date.month-1,data.index.days_in_month[-2])]
+
+    output_file("templates\lines.html")
+
+    p = figure(title="Interactive stock chart", x_axis_label='Date', x_axis_type="datetime", y_axis_label='Close price, USD')
+    p.line(last_mo_data['Close'].index, last_mo_data['Close'], legend=stock_symbol, line_width=2)
+
+    save(p) #save(p)
+    return None
+
+def insert_plot_into_html(orig_template, bare_plot_file, output_html):
+    lines=load_file_as_list(orig_template)
+    headerlines=lines[0:6]
+    lines=lines[6:]
+    
+    #### after <title>:
+    templines=[]
+    templines.append('<link rel="stylesheet" href="https://cdn.pydata.org/bokeh/release/bokeh-0.12.1.min.css" type="text/css" />\n')
+    templines.append('\n')        
+    templines.append('<script type="text/javascript" src="https://cdn.pydata.org/bokeh/release/bokeh-0.12.1.min.js"></script>')
+    templines.append('<script type="text/javascript">')
+    templines.append('    Bokeh.set_log_level("info");')    
+    
+    lines=lines[:-2] #remove closing body and html tags
+
+    #### after </form>
+    templines2=load_file_as_list(bare_plot_file) 
+    templines2=templines2[27:]
+    #print('ttt2=',templines2)
+    lines = headerlines + templines + lines + templines2
+
+##    lines.append('    </body>\n')
+##    lines.append('</html>')
+    write_string_to_file(output_html, "".join(lines))
+    return None
+
+if __name__ == "__main__":
+    stock_symbol = raw_input("enter stock: ")
+    get_stock_price(stock_symbol)
+    insert_plot_into_html("templates\index.html", "templates\lines.html", "templates\plot.html")
+
