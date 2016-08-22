30b00b318d98daacdab16b43d318c23044899a2f
AT <ewzarp@gmail.com>
2016-08-21 20:30:10 -0400

    manage API keys through an ENV VAR

--- quandl_py_direct.py
+++ quandl_py_direct.py
@@ -23,7 +23,7 @@ def write_string_to_file(CURR_FILENAME, string_to_write):
     return None
 
 def get_stock_price(stock_symbol):
-    quandl.ApiConfig.api_key = 'YOUR QUANDL API CONFIG KEY HERE' 
+    quandl.ApiConfig.api_key = MY_QUANDL_API_KEY
     data = quandl.get("WIKI/" + stock_symbol)
 
     col_names=list(data)
