889a07050fe2e56a38e460a6f697725630c30559
AT <ewzarp@gmail.com>
2016-08-21 20:36:00 -0400

    API heroku env fix

--- quandl_py_direct.py
+++ quandl_py_direct.py
@@ -23,7 +23,7 @@ def write_string_to_file(CURR_FILENAME, string_to_write):
     return None
 
 def get_stock_price(stock_symbol):
-    quandl.ApiConfig.api_key = MY_QUANDL_API_KEY
+    quandl.ApiConfig.api_key = os.environ.get(MY_QUANDL_API_KEY, None)
     data = quandl.get("WIKI/" + stock_symbol)
 
     col_names=list(data)
